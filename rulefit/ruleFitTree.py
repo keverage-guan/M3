9c095b7625fbf985644926720699d4a895ce5d39
BevinTheBeverage <38870147+BevinTheBeverage@users.noreply.github.com>
2022-02-26 21:26:26 -0500

    repeat model to find lowest RSME

--- rulefit/ruleFitTree.py
+++ rulefit/ruleFitTree.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from rulefit import RuleFit
 
 # https://github.com/christophM/rulefit, has information for advantages and disadvantages
 # https://machinelearningmastery.com/different-results-each-time-in-machine-learning/, different results each time
 # About lasso regressions: https://www.statisticshowto.com/lasso-regression/
-from rulefit import RuleFit
 
 # coefficient (a) is the weight of the decision rule in the lasso regression
 # support (s) is the percentage of data points to which the decision rule applies
@@ -25,27 +25,47 @@ features = X.columns # set names of features
 
 X = X.to_numpy() # convert training to numpy array
 
-rf = RuleFit()
-rf.fit(X, y, feature_names = features) # generate decision rules using training data
+# train model to minimize RSME
+RSMEs = []
+bestrf = RuleFit()
+lowestRSME = 999
+# train model 10 times and store the one with the lowest RSME
+for i in range(10):
+    rf = RuleFit()
+    rf.fit(X, y, feature_names = features) # generate decision rules using training data
 
-# CHECKING MODEL ON TRAINING DATA
+    # CHECKING MODEL ON TRAINING DATA
 
+    predictions = rf.predict(X) # predict values
+    
+    training['prediction'] = predictions # add predictions to dataframe
+
+    # store results in separate data frame
+    training_results = training[['state', predicted_col, 'prediction']].copy()
+
+    # calculate RSME
+    n = training[training.columns[0]].count() # number of rows
+    # sum errors
+    error_sum = 0
+    training = training.reset_index(drop=True)  # make sure indexes pair with number of rows
+    for index, row in training.iterrows():
+        error_sum += (row[predicted_col] - row['prediction'])**2/n
+    RSME = np.sqrt(error_sum)
+    RSMEs.append(RSME)
+    if RSME < lowestRSME:
+        bestrf = rf
+        lowestRSME = RSME
+    training = training.drop("prediction", axis = 1)  # remove type column (all values are "rule")
+
+# print results of best model
+rf = bestrf
 predictions = rf.predict(X) # predict values
 training['prediction'] = predictions # add predictions to dataframe
-
 # store results in separate data frame and print
 training_results = training[['state', predicted_col, 'prediction']].copy()
 print(training_results.head())
-
-# calculate RSME
-n = training[training.columns[0]].count() # number of rows
-# sum errors
-error_sum = 0
-training = training.reset_index()  # make sure indexes pair with number of rows
-for index, row in training.iterrows():
-    error_sum += (row[predicted_col] - row['prediction'])/n
-RSME = np.sqrt(error_sum)
-print("RSME: ", RSME)
+print("Lowest RSME: ", lowestRSME)
+print("RSMEs: ", RSMEs)
 print()
 
 # PRINTING RULES OF MODEL AND IMPORTANCE OF FEATURES
